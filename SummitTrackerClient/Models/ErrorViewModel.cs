2731730dccaeebc2546385e0778f4435ad9f8ff9
Jacob <JacobJardineuni@gmail.com>
2023-09-08 14:52:22 +0100

    Add project files.

---
+++ SummitTrackerClient/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace SummitTrackerClient.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
